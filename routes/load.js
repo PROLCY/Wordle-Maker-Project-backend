0af02709483eb08bc7dad9b6b8dd4e62f8af23bc
PROLCY <ckd248@naver.com>
2022-11-16 15:22:16 +0900

    2022/11/16 GET /load, POST /load 라우터 구현

--- routes/load.js
+++ routes/load.js
@@ -5,31 +5,47 @@ const { Maker, Solver } = require('../models');
 
 const router = express.Router();
 
+const getSolvers = async ( makerNickname ) => {
+    const maker = await Maker.findOne({
+        where: {
+            nickname: makerNickname,
+        },
+        include: [{
+            model: Solver,
+            attributes: ['nickname', 'word_list'],
+        }]
+    });
+    const solvers = maker.Solvers.map(
+        solver => ({
+            nickname: [solver.nickname.split('').map(nickname => ({
+                text: nickname,
+                state: 'filled',
+            }))],
+            wordList: solver.word_list === null ? [] : JSON.parse(solver.word_list),
+        }));
+    return solvers;
+};
+
 router.get('/', async (req, res) => {
     try {
-        const maker = await Maker.findOne({
-            attributes: ['correct_word'],
-            where: {
-                nickname: 'AAAAA',
-            },
-            include: [{
-                model: Solver,
-                attributes: ['nickname', 'word_list'],
-            }]
-        });
-        const solvers = maker.Solvers.map(
-            solver => ({
-                nickname: [solver.nickname.split('').map(nickname => ({
-                    text: nickname,
-                    state: 'filled',
-                }))],
-                wordList: solver.word_list === null ? [] : JSON.parse(solver.word_list),
-            }))
-        res.send(solvers);
+        if ( !req.session.maker ) {
+            res.send('no-session');
+            return;
+        }
+        res.send(await getSolvers(req.session.maker));
     } catch (error) {
         console.error(error);
     }
 });
+
+router.post('/', async (req, res) => {
+    try {
+        req.session.maker = req.body.makerNickname;
+        res.send(await getSolvers(req.body.makerNickname));
+    } catch (error) {
+        console.error(error);
+    }
+})
 
 router.post('/exist', async (req, res) => {
     try {
